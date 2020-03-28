61c7d064f8c0c4bce0219a16a1920b4fa3eedcaa
João Pablo <jp.pablolima@gmail.com>
2020-03-28 11:42:47 -0300

    criaçao das ongs no DB e Listar

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,35 @@
 const express = require('express');
+const crypto = require('crypto');
+
+const connection = require('./database/connection');
+
+
 const routes = express.Router();
 
+routes.get('/ongs', async(request, response) => { 
+    const ongs = await connection('ongs').select('*');
 
-routes.post('/users',(request, response) =>  {
-    const body = request.body;
- 
-    console.log(body);
+    return response.json(ongs);
+});
 
-    return response.json({
-        evento: "Oi, eu sou o Goku",
-        nome: 'Goku'
+
+
+
+routes.post('/ongs', async (request, response) =>  {
+    const { name, email, whatsapp, city, uf } = request.body;
+
+    const id = crypto.randomBytes(4).toString('HEX');
+
+    await connection('ongs').insert({
+        id,
+        name, 
+        email,
+        whatsapp,
+        city,
+        uf,
     });
+
+    return response.json({ id });
 }); 
 
 module.exports = routes;
