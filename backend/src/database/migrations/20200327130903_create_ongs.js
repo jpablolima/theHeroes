a9b94b1a826838624bd7982bda266bba60480532
João Pablo <jp.pablolima@gmail.com>
2020-03-28 10:59:24 -0300

    criaçao das table no DB

--- backend/src/database/migrations/20200327130903_create_ongs.js
+++ backend/src/database/migrations/20200327130903_create_ongs.js
@@ -1,16 +1,16 @@
 
 exports.up = function(knex) {
-  knex.schema.createTable('ongs', function(table) {
+  return knex.schema.createTable('ongs', function(table) {
     table.string('id').primary();
     table.string('name').notNullable();
     table.string('email').notNullable();
     table.string('whatsapp').notNullable();
     table.string('city').notNullable();
     table.string('uf',2).notNullable();
-    //table.string('id').notNullable();
+    
   });
 };
 
 exports.down = function(knex) {
-  knex.schema.dropTable('ongs');
+  return knex.schema.dropTable('ongs');
 };
